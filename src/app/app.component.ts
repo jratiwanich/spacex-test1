3a0b74e87fd25fe036c8bd085db1e9a5a4bedde4
Jak Ratiwanich <jratiwanich@schoolsfirstfcu.org>
2018-03-30 13:04:31 -0700

    added compiled files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,9 @@ import { SpcxRestService} from './spcx-rest.service';
 
 export class AppComponent {
   title = 'app';
+  entireObj: any;
+  createDate: Date;
+  modDate: Date;
   features: any[] = [];
     constructor(
       private appSettingsService : SpcxRestService 
@@ -17,7 +20,12 @@ export class AppComponent {
   ngOnInit(){
     this.appSettingsService.getJSON().subscribe(data => {
           console.log("data=",data);
+          this.entireObj = data;
+          //label something on the page
           this. title = data.label;
+          this.createDate = data['dcmi:created'];
+          this.modDate = data['dcmi:modified'];
+          //list out all the features inside json object
           for(let result of data.features){
             this.features.push(result);
          }
